586f6ad5090055d83f423d8084109192a7e46a0f
Ben Cornelis <bencornelis02@gmail.com>
2015-07-10 12:54:33 -0700

    let users edit comments; fix signup parameters bug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,6 +18,22 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      flash[:notice] = "Comment successfully updated."
+      redirect_to post_path(@comment.post)
+    else
+      flash[:alert] = "Unable to update comment, try again."
+      render :edit
+    end
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text)
